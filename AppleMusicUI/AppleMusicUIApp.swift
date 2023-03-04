7993eb51e6b449199e4bb84422d869dc5c0c7c57
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-03-04 21:48:08 +0300

    Initial Commit

---
+++ AppleMusicUI/AppleMusicUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicUIApp.swift
+//  AppleMusicUI
+//
+//  Created by Abdullah Karaboğa on 4.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
